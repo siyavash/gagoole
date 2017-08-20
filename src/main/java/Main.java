e17233c285c29b57ac3c4f90b8a596baa8ab5b8d
Amirzia <amir_zia@outlook.com>
2017-08-20 12:23:49 +0430

    add properties file for number of threads and run mode AND edit Main for reading properties

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,52 @@
 import kafka.KafkaSubscribe;
+import kafka.Seeds;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Main {
+    private static int NTHREADS;
+    private static boolean initialMode;
+
     public static void main(String[] args) throws Exception {
+        loadProperties();
+
+        if (initialMode)
+            Seeds.publish();
+
         KafkaSubscribe kafkaSubscribe = new KafkaSubscribe();
         kafkaSubscribe.start();
+
         Crawler crawler = new Crawler(kafkaSubscribe);
-        crawler.setThreads(200);
+        crawler.setThreads(NTHREADS);
         crawler.start();
+    }
+
+    private static void loadProperties() {
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try {
+
+            input = new FileInputStream("config.properties");
+            prop.load(input);
+
+            NTHREADS = Integer.getInteger(prop.getProperty("threads-number", "500"));
+            initialMode = prop.getProperty("initial-mode", "true").equals("true");
+
+        } catch (IOException ex) {
+            System.err.println("error in reading config file:");
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
