68c663e6f1f544588525f4246319acc698864f66
Amirzia <amir_zia@outlook.com>
2017-08-20 14:57:40 +0430

    add resource directory

---
+++ src/main/java/Connector.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Amir on 8/20/2017 AD.
+ */
+public class Connector {
+}
---
+++ src/main/java/Util/Logger.java
@@ -0,0 +1,7 @@
+package Util;
+
+/**
+ * Created by Amir on 8/20/2017 AD.
+ */
+public class Logger {
+}
