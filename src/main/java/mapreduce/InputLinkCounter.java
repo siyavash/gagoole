3754164fb5aae910964f53c5c085c8530621e0be
Siyavash Najafzade <siyavash.najafzade@gmail.com>
2017-09-02 13:05:58 +0430

    set configuration for mapreduce job

--- src/main/java/mapreduce/InputLinkCounter.java
+++ src/main/java/mapreduce/InputLinkCounter.java
@@ -1,7 +1,18 @@
 package mapreduce;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.mapreduce.Job;
+
+import java.io.IOException;
+
 public class InputLinkCounter {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        Configuration configuration = HBaseConfiguration.create();
+        configuration.set("hbase.zookeeper.property.clientPort", "2181");
+        configuration.set("hbase.zookeeper.quorum", "localmaster");
+        Job job = Job.getInstance(configuration, "InputLinkCounter Job");
+        job.setJarByClass(mapreduce.InputLinkCounter.class);
     }
 }
