44f118a61c20c278f221e7799b6e9f1ba114edf0
Siyavash Najafzade <siyavash.najafzade@gmail.com>
2017-09-02 13:24:58 +0430

    crate mapper and reducer class

--- src/main/java/mapreduce/InputLinkCounter.java
+++ src/main/java/mapreduce/InputLinkCounter.java
@@ -2,17 +2,45 @@ package mapreduce;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableMapper;
+import org.apache.hadoop.hbase.mapreduce.TableReducer;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 
 public class InputLinkCounter {
 
     public static void main(String[] args) throws IOException {
-        Configuration configuration = HBaseConfiguration.create();
-        configuration.set("hbase.zookeeper.property.clientPort", "2181");
-        configuration.set("hbase.zookeeper.quorum", "localmaster");
-        Job job = Job.getInstance(configuration, "InputLinkCounter Job");
+        Configuration hbaseConfiguration = HBaseConfiguration.create();
+        hbaseConfiguration.set("hbase.zookeeper.property.clientPort", "2181");
+        hbaseConfiguration.set("hbase.zookeeper.quorum", "localmaster");
+        Job job = Job.getInstance(hbaseConfiguration, "InputLinkCounter Job");
         job.setJarByClass(mapreduce.InputLinkCounter.class);
+    }
+
+    public static class Mapper extends TableMapper<ImmutableBytesWritable, IntWritable> {
+
+        @Override
+        protected void map(ImmutableBytesWritable key,
+                           Result value,
+                           Context context) throws IOException, InterruptedException {
+
+        }
+    }
+
+    public static class Reducer extends TableReducer<ImmutableBytesWritable, InputLinkCounter, Put> {
+
+        @Override
+        protected void reduce(ImmutableBytesWritable key,
+                              Iterable<InputLinkCounter> values,
+                              Context context) throws IOException, InterruptedException {
+            
+        }
     }
 }
