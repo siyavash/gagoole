db301b25d0f89e5a71bb17fde54212b204fc3a63
Hossein Keshavarz <hoseinkeshavarz1997@gmail.com>
2017-09-03 21:21:51 +0430

    find new bugs

--- src/main/java/TimeoutThread.java
+++ src/main/java/TimeoutThread.java
@@ -17,28 +17,28 @@ public class TimeoutThread extends Thread
         while(true) {
             callPair = null;
             try {
-                System.out.print("1 ");
+//                System.out.print("1 ");
                 callPair = linkedBlockingQueue.take();
-                System.out.print("2 ");
+//                System.out.print("2 ");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             if (callPair != null) {
-                System.out.print("3 ");
+//                System.out.print("3 ");
                 call = callPair.getKey();
-                System.out.print("4 ");
+//                System.out.print("4 ");
             }
             timeDifference = System.currentTimeMillis() - callPair.getValue();
             try {
                 if (timeDifference <= 1500){
-                    System.out.print("5 ");
+//                    System.out.print("5 ");
                     Thread.sleep(1500 - timeDifference);
-                    System.out.print("6 ");
+//                    System.out.print("6 ");
                 }
                 if (call != null && !call.isCanceled()) {
-                    System.out.print("7 ");
+//                    System.out.print("7 ");
                     call.cancel();
-                    System.out.println("8");
+//                    System.out.println("8");
                 }
             } catch (InterruptedException e) {
 
