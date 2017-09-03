a3b6037bfe6b8e52df630b8f6e837dce1ab44fde
Hossein Keshavarz <hoseinkeshavarz1997@gmail.com>
2017-09-03 19:23:04 +0430

    add sout to find problem

--- src/main/java/TimeoutThread.java
+++ src/main/java/TimeoutThread.java
@@ -17,20 +17,28 @@ public class TimeoutThread extends Thread
         while(true) {
             callPair = null;
             try {
+                System.out.println("1");
                 callPair = linkedBlockingQueue.take();
+                System.out.println("2");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             if (callPair != null) {
+                System.out.println("3");
                 call = callPair.getKey();
+                System.out.println("4");
             }
             timeDifference = System.currentTimeMillis() - callPair.getValue();
             try {
                 if (timeDifference <= 1500){
+                    System.out.println("5");
                     Thread.sleep(1500 - timeDifference);
+                    System.out.println("6");
                 }
                 if (call != null && !call.isCanceled()) {
+                    System.out.println("7");
                     call.cancel();
+                    System.out.println("8");
                 }
             } catch (InterruptedException e) {
 
