5d5bec8a262b2b5588044189f71450a81b3ba384
Siyavash Najafzade <siyavash.najafzade@gmail.com>
2017-09-04 17:14:27 +0430

    create AnchorsCounter class

---
+++ src/main/java/mapreduce/AnchorsCounter.java
@@ -0,0 +1,11 @@
+package mapreduce;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+
+public class AnchorsCounter extends Configured implements Tool {
+    @Override
+    public int run(String[] args) throws Exception {
+        return 0;
+    }
+}
