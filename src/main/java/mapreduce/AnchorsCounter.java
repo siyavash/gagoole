22efa66157e7133da5685fb8e72d9fd09790da21
Siyavash Najafzade <siyavash.najafzade@gmail.com>
2017-09-04 17:26:43 +0430

    create Job and Scan object for mapreduce

--- src/main/java/mapreduce/AnchorsCounter.java
+++ src/main/java/mapreduce/AnchorsCounter.java
@@ -1,11 +1,28 @@
 package mapreduce;
 
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.util.Tool;
 
 public class AnchorsCounter extends Configured implements Tool {
+
+    private static final byte[] COLUMN_FAMILY = "cf".getBytes();
+    private static final byte[] SUB_LINKS = "subLinks".getBytes();
+
     @Override
     public int run(String[] args) throws Exception {
+        Job job = Job.getInstance(getConf(), "AnchorsCounter");
+        job.setJarByClass(AnchorsCounter.class);
+
+        Scan scan = new Scan();
+
+        scan.setCaching(500);
+        scan.setCacheBlocks(false);
+
+        scan.addColumn(COLUMN_FAMILY, SUB_LINKS);
+
+        
         return 0;
     }
 }
