1ba9d63bf60573863cec6dd40f669175252afaac
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2017-09-12 04:12:50 +0430

    Kill program after given time

--- src/main/java/Gagoole.java
+++ src/main/java/Gagoole.java
@@ -6,6 +6,20 @@ public class Gagoole {
 
         Crawler crawler = new Crawler();
         crawler.start();
+
+        if (args.length != 0)
+        {
+            new Thread(() -> {
+                try
+                {
+                    Thread.sleep(Long.parseLong(args[0]) * 1000);
+                    crawler.close();
+                } catch (InterruptedException ignored)
+                {
+
+                }
+            }).start();
+        }
     }
 
 }
