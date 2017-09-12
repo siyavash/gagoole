743ce3bb92167e3f03aee44bd72cca92b5b3cff7
Hossein Keshavarz <hoseinkeshavarz1997@gmail.com>
2017-09-12 12:35:14 +0430

    comment gagoole

--- src/main/java/Gagoole.java
+++ src/main/java/Gagoole.java
@@ -7,19 +7,19 @@ public class Gagoole {
         Crawler crawler = new Crawler();
         crawler.start();
 
-        if (args.length != 0)
-        {
-            new Thread(() -> {
-                try
-                {
-                    Thread.sleep(Long.parseLong(args[0]) * 1000);
-                    crawler.close();
-                } catch (InterruptedException ignored)
-                {
-
-                }
-            }).start();
-        }
+//        if (args.length != 0)
+//        {
+//            new Thread(() -> {
+//                try
+//                {
+//                    Thread.sleep(Long.parseLong(args[0]) * 1000);
+//                    crawler.close();
+//                } catch (InterruptedException ignored)
+//                {
+//
+//                }
+//            }).start();
+//        }
     }
 
 }
