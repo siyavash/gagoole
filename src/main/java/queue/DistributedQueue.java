21d3b0b7ae6917fc8c52816e3ba02c28166a3c00
Hossein Keshavarz <hoseinkeshavarz1997@gmail.com>
2017-09-12 09:26:15 +0430

    test new table

--- src/main/java/queue/DistributedQueue.java
+++ src/main/java/queue/DistributedQueue.java
@@ -21,7 +21,7 @@ public class DistributedQueue extends Thread implements URLQueue {
     private final ArrayBlockingQueue<String> arrayBlockingQueue = new ArrayBlockingQueue<>(5000);
     private Properties publishProps = new Properties();
     private Properties consumeProps = new Properties();
-	private final String groupId = "Perspolis";
+	private final String groupId = "Salam";
 
     public DistributedQueue(String bootstrapServers, String topicName) {
         this.topicName = topicName;
